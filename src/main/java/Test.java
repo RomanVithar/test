929b3c3bb64b366f46ef54df4f57c2af59ed690a
RomanVithar <an.roman.b@gmail.com>
2021-01-13 19:38:28 +0300

    c

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+import graph.realistion.SimpleGraph;
+
+public class Test {
+    public static void main(String[] args) {
+        SimpleGraph graph = new SimpleGraph();
+        graph.insert("fsd");
+    }
+}
